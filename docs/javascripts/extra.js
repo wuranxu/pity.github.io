4e02e28a30aab9910c360e7297bb5a177f12fc63
rx.wu <rx.wu@trip.com>
2021-12-15 21:31:27 +0800

    添加关键字

---
+++ docs/javascripts/extra.js
@@ -0,0 +1,9 @@
+const btw = new BTWPlugin();
+document.getElementsByTagName("article")[0].id = "container";
+btw.init({
+    id: 'container',
+    blogId: '28585-1639573004975-308',
+    name: '米洛的测开日记',
+    qrcode: 'https://gitee.com/woodywrx/picture/raw/master/2021-12-15/1639572832808-qrcode_for_gh_f52fb2135f68_258.jpg',
+    keyword: 'tester',
+});
