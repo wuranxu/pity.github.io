2e20c4f52d9763efd9d3e4bc417ca8a847b5e02f
wuranxu <619434176@qq.com>
2022-04-15 22:01:01 +0800

    改为static

--- docs/index.md
+++ docs/index.md
@@ -4,7 +4,7 @@
 
     个人技术公众号: `米洛的测开日记`，欢迎大家关注我，掌握最新测试开发知识。
 
-![](http://oss.pity.fun/picture/2022-1-1/1641020334827-qrcode_for_gh_f52fb2135f68_430.jpg)
+![](https://static.pity.fun/picture/2022-1-1/1641020334827-qrcode_for_gh_f52fb2135f68_430.jpg)
 
 !!! Example "关于Pity平台"
 
--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -4,6 +4,6 @@ btw.init({
     id: 'container',
     blogId: '28585-1639573004975-308',
     name: '米洛的测开日记',
-    qrcode: 'http://oss.pity.fun/picture/2021-12-15/1639572832808-qrcode_for_gh_f52fb2135f68_258.jpg',
+    qrcode: 'https://static.pity.fun/picture/2021-12-15/1639572832808-qrcode_for_gh_f52fb2135f68_258.jpg',
     keyword: 'tester',
 });
